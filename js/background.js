2f90d42caa507837432ad147ec8d62c15b3b2d8b
GUNWOO3666 <gunwoo3666@gmail.com>
2023-11-21 00:53:48 +0900

    plus

--- js/background.js
+++ js/background.js
@@ -3,7 +3,7 @@
 // 핵심기능: array에서 랜덤한 숫자를 얻어 숫자에 따른 이미지를 body에 추가함
 
 const images = [
-    "1.jpg", "2.jpg", "3.jpg", "4.jpg"
+    "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"
 ]
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
